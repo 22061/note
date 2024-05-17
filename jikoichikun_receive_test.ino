ed4559eb6db2df4b6603efca61c7f5b9c770c6c2
22061 <150252996+22061@users.noreply.github.com>
2024-05-17 16:51:13 +0900

    Update

--- jikoichikun_receive_test.ino
+++ jikoichikun_receive_test.ino
@@ -35,7 +35,7 @@ namespace zk_api {
     digitalWrite(7,status);
   }
   inline void rear_right_cw_ccw(int status){
-    digitalWrite(8,status);
+    digitalWrite(12,status);
   }
 
   inline void control_front_right(int power){
